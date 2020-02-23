9f6185da3015b591a19959f5e84382d07ab64d1f
Helmut Kemper <helmut.kemper@gmail.com>
2020-02-22 22:23:29 -0300

    trabalhando

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -18,53 +18,8 @@ func main() {
 
 	var db iotmakerDbInterface.DbFunctionsInterface
 	var err error
-	var nodes, ways, relations, others int
 
-	var lat, lon float64
 	importMap := iotmaker_geo_pbf_import.Import{}
-	importMap.AppendNodeToFile("./bin", 1, 1.1, 1.2)
-	importMap.AppendNodeToFile("./bin", 2, 2.1, 2.2)
-	importMap.AppendNodeToFile("./bin", 3, 3.1, 3.2)
-	importMap.AppendNodeToFile("./bin", 4, 4.1, 4.2)
-	importMap.AppendNodeToFile("./bin", 5, 5.1, 5.2)
-	importMap.AppendNodeToFile("./bin", 6, 6.1, 6.2)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 1)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 2)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 3)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 4)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 5)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 6)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	err, lon, lat = importMap.FindLonLatByIdInFile("./bin", 7)
-	fmt.Printf("err: %v\n", err)
-	fmt.Printf("lon: %v\n", lon)
-	fmt.Printf("lat: %v\n", lat)
-
-	return
 	db = &iotmakerDbMongodb.DbFunctions{}
 	err = db.Connect("mongodb://0.0.0.0:27017", "geo", []string{"point", "way", "polygon"})
 	if err != nil {
@@ -74,16 +29,22 @@ func main() {
 	//      /media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/nodes/13787735.xml
 
 	importMap = iotmaker_geo_pbf_import.Import{}
-	err, nodes, ways, relations, others = importMap.CountElements("/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf")
+	/*err, nodes, ways, relations, others = importMap.CountElements("/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf")
 	if err != nil {
 		log.Fatalf("db.connection.error: %v", err.Error())
 	}
 	log.Printf("nodes: %v\n", nodes)
 	log.Printf("ways: %v\n", ways)
 	log.Printf("relations: %v\n", relations)
-	log.Printf("others: %v\n", others)
+	log.Printf("others: %v\n", others)*/
 
-	importMap.ExtractNodes("/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf", "/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/nodesJSon")
+	//importMap.ExtractNodesManager("/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf", "/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/nodesBin")
+	err = importMap.FindAllNodesFrTest("/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf", "/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/nodesBin")
+	if err != nil {
+		fmt.Printf("%v", err.Error())
+	}
+
+	fmt.Println("terminou ok")
 	//iotmaker_geo_pbf_import.ProcessPbfFileInMemory(db, "/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/portugal-latest.osm.pbf", "/media/kemper/c5d4fd1f-1a7e-4bdd-8124-e2ad60e187761/binMap.bin")
 }
 
